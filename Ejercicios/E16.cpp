80f68371e079fef2517da0668e04a0a2b25374ef
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-06 16:31:48 -0600

    TAREAS 5695

---
+++ Ejercicios/E16.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n, me15,ma50,num25_45;
+}
---
+++ Ejercicios/E5.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+float costo,total,IVA,precioT,desc;
+}
