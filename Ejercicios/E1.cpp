c787200d48f4ef761f45aa837ab712588cdd007f
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-20 14:58:20 -0600

    modificado E1

--- Ejercicios/E1.cpp
+++ Ejercicios/E1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-int par,impar,n;
+int par=0,impar=0,n;
+for(n=1;n<=200;n++){
+if(n%2==0){
+par=par+n;}else{impar=impar+n;}}
+cout<<"la suma de todos los pares entre 1 y 200 es: "<<par<<endl;
+cout<<"la suma de todos los impares entre 1 y 200 es: "<<impar<<endl;
 }
 
