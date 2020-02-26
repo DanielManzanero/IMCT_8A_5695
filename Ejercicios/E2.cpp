fa1a27256c97060abdd03f3cd661a0928776fabf
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-25 23:59:18 -0600

    modificado E2

--- Ejercicios/E2.cpp
+++ Ejercicios/E2.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-int x,n,num;
+cout<<"Ingrese la rerie de numeros, el ultimo debe ser -99"<<endl;
+int x,num=-99;
+	do{
+	cin>>x;
+	if(x>num){
+	num=x;
+	}
+	}while(x!=-99);
+cout<<"El mayor de la reie es: "<<num<<endl;
+return 0;
 }
 
