777a995c44f7abce0de07d8ec6fc7e960309c612
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-26 01:12:09 -0600

    modificado E3

--- Ejercicios/E3.cpp
+++ Ejercicios/E3.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
+int suma=0;
+unsigned long long producto=1;
+
+
+int calcular(){
+        for(int a=20;a<=400;a+=2){
+         if(a%2==0){
+         producto=producto*a;
+         suma+=a;
+         }
+        }
+return 0;
+}
+
+
+
 int main(){
-int suma,n;
-long producto;
+calcular();
+cout<<"La suma de los numeros pares comprendidos entre 20 y 400 inclusive es: "<<suma<<endl;
+cout<<"Y el producto de esos numeros es: "<<"EL NUMERO ES MAS GRANDE QUE UN UNSIGNED LONG LONG, ES ENORME Y NO ENCONTRE COMO EXPRESARLO :'C"<<endl;
+return 0;
 }
 
