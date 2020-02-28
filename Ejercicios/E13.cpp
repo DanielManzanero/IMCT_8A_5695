599394cd55de8563d7b7b27d729f3b9a88233b73
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-27 22:16:48 -0600

    modificado E13

--- Ejercicios/E13.cpp
+++ Ejercicios/E13.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-float prom, notaA;
-int alumnos,notas;
+float prom, notaA, suma=0;
+int n;
+cout<<"Ingrese el total de alumnos"<<endl;
+cin>>n;
+for(int a=1;a<=n;a++){
+cin>>notaA;
+suma+=notaA;
+}
+prom=suma/n;
+cout<<"Promedio: "<<prom<<endl;
+return 0;
 }
