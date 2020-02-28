b934bd453b4b6d14dee861f00c0432fdda0ea88c
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-27 23:05:40 -0600

    modificado E4

--- Ejercicios/E4.cpp
+++ Ejercicios/E4.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-int total,n,x;
+int total=0,x;
+cout<<"Ingrese 500 numeros enteros"<<endl;
+for(int n=1;n<=500;n++){
+cin>>x;
+if(x>0)
+total++;
+}
+cout<<total<<" numeros son positivos"<<endl;
+return 0;
 }
