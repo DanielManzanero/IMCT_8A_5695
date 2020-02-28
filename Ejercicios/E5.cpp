94d5dce7b545ab00bfaba003b1cc951da27e0807
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-27 23:17:52 -0600

    modificando E5

--- Ejercicios/E5.cpp
+++ Ejercicios/E5.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-int n;
-float costo,total,IVA,precioT,desc;
+unsigned int unid;
+float cant,total,IVA,pors,desc;
+cin>>cant>>unid;
+total=cant*unid;
+cout<<"Total: "<<total<<endl;
+IVA=total*.15;
+cout<<"IVA: "<<IVA<<endl;
+pors=total+IVA;
+cout<<"Total con IVA: "<<pors<<endl;
+if(pors>50000){
+desc=pors*.5;
+cout<<"Total con descuento: "<<pors-desc<<endl;}
+else{
+cout<<"sin descuento"<<endl;}
+return 0;
 }
