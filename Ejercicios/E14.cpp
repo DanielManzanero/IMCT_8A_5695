ed6b2b3f5a27b775a6b1132e28c07c68e9420f46
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-27 22:26:40 -0600

    modificado E14

--- Ejercicios/E14.cpp
+++ Ejercicios/E14.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-int suma,n;
+int suma=0;
+for(int n=2;n<=20;n+=2){
+suma+=n;
+}
+cout<<"La suma de los primeros 10 numeros pares es: "<<suma<<endl;
+return 0; 
 }
 
