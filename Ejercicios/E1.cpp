d23f7718a5d040872f239d2ed112abd382ebfe61
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-27 19:33:40 -0600

    modificado E1

--- Ejercicios/E1.cpp
+++ Ejercicios/E1.cpp
@@ -7,5 +7,5 @@ if(n%2==0){
 par=par+n;}else{impar=impar+n;}}
 cout<<"la suma de todos los pares entre 1 y 200 es: "<<par<<endl;
 cout<<"la suma de todos los impares entre 1 y 200 es: "<<impar<<endl;
-}
 
+}
