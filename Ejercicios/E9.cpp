3202de1576c4830eeb36971077c659ff2af83159
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-27 21:25:01 -0600

    modificado E9

--- Ejercicios/E9.cpp
+++ Ejercicios/E9.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-int n;
-float media;
+int n=50,s=0;
+float media=0;
+cout<<"Ingresar "<<n<<" numeros"<<endl;
+for(int a=1;a<=n;a++){
+cin>>s;
+media+=s;
+}
+media=media/n;
+cout<<"La media es: "<<media<<endl;
+return 0; 
 }
