942f1bd00b0dbfe3fb62acdb897970d33073e7f8
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-02-27 20:11:48 -0600

    modificado E7

--- Ejercicios/E7.cpp
+++ Ejercicios/E7.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main(){
 int suma=0;
-for(int n=2;
+for(int n=2;n<=100;n+=2){
+suma=suma+n;
+}
+cout<<"La suma de los numeros pares del 2 al 100 es: "<<suma<<endl;
+return 0;
 }
