345a47bd931072771467054afdf64067f38fb422
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-03-24 00:06:06 -0600

    Actualizar

--- Ejercicios/E10.cpp
+++ Ejercicios/E10.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
-int main(){
-int n,x;
+int n=0,m=0,t=0;
+void ingresar(){
+cin>>n;
+return;
 }
+
+void calcular(){
+    ingresar();
+    cout<<"Los múltiplos son:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        m=4*t++;
+        cout<<m<<endl;
+    }
+return;
+}
+
+int main(){
+    cout<<"Ingrese la cantidad de múltiplos de 4 que desee"<<endl;
+    calcular();
+return 0;
+}
+
+
+
+
--- Ejercicios/E15.cpp
+++ Ejercicios/E15.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 int main(){
-int suma,n; 
+srand(time(NULL));
+int l_i=-20,l_s=20,suma=0;
+	for(int i=0;i<20;i++){
+	int valor =l_i + rand()%(l_s + 1 - l_i);
+	cout<<valor<<endl;
+	if(valor>0){
+	suma+=valor;
+	}
+}
+     int valor=rand()%100;	
+cout<<"Suma es: "<<suma<<endl;
+return 0;
 }
