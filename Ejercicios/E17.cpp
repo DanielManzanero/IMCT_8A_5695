e9c5f45095187d1244913516b15103cf0a03cc7d
DANIEL JESUS MANZANERO VAZQUEZ <danieljesusmanzanero@hotmail.com>
2020-03-23 23:54:53 -0600

    actualizar

--- Ejercicios/E17.cpp
+++ Ejercicios/E17.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
 using namespace std;
-int main(){
-int suma,n;
+
+int N1=0,N2=0,suma=0;
+
+void registro (){
+    cin>>N1;
+return;
 }
+
+void ciclo (){
+    for (int i = 0; i < 10; i++)
+    {
+        registro();
+        if (N1 > N2)
+        {
+            N2 = N1;
+            suma = N2 + suma;
+        }
+        else
+        {
+            cout<<"La suma no se podrá hacer porque los números que ingresaste no están ordenados"<<endl;
+            return;
+        }        
+    }
+return;    
+}
+
+int main(){
+    cout<<"Ingrese 10 números enteros"<<endl;
+    ciclo();
+    cout<<"La suma de sus números es: "<<suma<<endl;
+return 0;
+}
+
+
